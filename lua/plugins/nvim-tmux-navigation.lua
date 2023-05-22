cdd04d734650a83a7d5af98fd64777f2edce318a
liaoliaopro <liaoliaopro@gmail.com>
2023-05-22 19:41:45 +0800

    feat: work smoothly with tmux

---
+++ lua/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "alexghergh/nvim-tmux-navigation",
+    -- 'christoomey/vim-tmux-navigator',
+
+    keys = {
+      { "<C-h>", "<Cmd>NvimTmuxNavigateLeft<Cr>" },
+      { "<C-j>", "<Cmd>NvimTmuxNavigateDown<Cr>" },
+      { "<C-k>", "<Cmd>NvimTmuxNavigateUp<Cr>" },
+      { "<C-l>", "<Cmd>NvimTmuxNavigateRight<Cr>" },
+      -- { "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<Cr>" },
+      -- { "<C-Space>", "<Cmd>NvimTmuxNavigateNext<Cr>" },
+    },
+    opts = {
+      disable_when_zoomed = true,
+    },
+  },
+}
