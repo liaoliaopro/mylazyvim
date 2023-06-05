ae8babb8825c90c79710ad627622a6c705032766
liaoliaopro <liaoliaopro@gmail.com>
2023-06-05 10:18:47 +0800

    remove plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.util.project" },
+    -- { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.test.core" },
 
     -- import/override with your plugins
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- set default python
+vim.g.python3_host_prog = "~/.rye/shims/python3"
+vim.g.python_host_prog = "~/.rye/shims/python"
