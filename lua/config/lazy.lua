b1536fbba3173deb04dfc4c5ad61067deaa1aeb8
liaoliaopro <liaoliaopro@gmail.com>
2023-06-13 18:48:14 +0800

    comment test plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.test.core" },
+    -- { import = "lazyvim.plugins.extras.test.core" },
     -- { import = "lazyvim.plugins.extras.dap.core" },
 
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,7 +1,13 @@
+--[[
+
 return {
   { "nvim-neotest/neotest-plenary" },
+  { "nvim-neotest/neotest-rust" },
+  { "nvim-neotest/neotest-python" },
   {
     "nvim-neotest/neotest",
     opts = { adapters = { "neotest-plenary", "neotest-rust", "neotest-go", "neotest-python" } },
   },
 }
+
+--]]
