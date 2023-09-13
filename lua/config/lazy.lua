2b380b51e87086e8ad3f989fbcddd2945fd18bfc
liaoliaopro <liaoliaopro@gmail.com>
2023-09-13 18:26:10 +0800

    feat: venv selector

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
     -- { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -19,7 +19,19 @@ return {
       })
     end,
   },
-
+  {
+    "linux-cultist/venv-selector.nvim",
+    cmd = "VenvSelect",
+    opts = {
+      name = {
+        "venv",
+        ".venv",
+        "env",
+        ".env",
+      },
+    },
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+  },
   {
     "neovim/nvim-lspconfig",
     opts = {
