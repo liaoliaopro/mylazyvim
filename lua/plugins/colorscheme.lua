a4ff4f1e8c0318d05172f054fcf882983f236a74
liaoliaopro <liaoliaopro@gmail.com>
2024-06-12 13:46:25 +0800

    tweak plugins

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- add kanagawa
+  { "rebelot/kanagawa.nvim" },
+
+  -- add nordic
+  { "AlexvZyl/nordic.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
