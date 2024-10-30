5501d64db8b881a0012b4da496bf61742d09c0e6
liaoliaopro <liaoliaopro@gmail.com>
2024-10-30 16:49:05 +0800

    plugins update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,6 @@ vim.opt.undofile = false
 -- set default python
 vim.g.python3_host_prog = "~/.rye/shims/python3"
 vim.g.python_host_prog = "~/.rye/shims/python"
+
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basedpyright"
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, {
-                "toml",
-                "svelte",
-                "css",
-                "http",
-                "json",
-                "proto",
-            })
-        end,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "toml",
+        "svelte",
+        "css",
+        "http",
+        "json",
+        "proto",
+        "hurl",
+      })
+    end,
+  },
 }
